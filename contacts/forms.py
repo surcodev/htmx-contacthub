e58a13ae294a49fc9dfb1c563362aad662a988db
richarq-fisi <temporal98x@gmail.com>
2025-11-02 18:22:04 -0500

    domingo

--- contacts/forms.py
+++ contacts/forms.py
@@ -1,1 +1,38 @@
 from django import forms
+from .models import Contact
+
+
+
+class ContactForm(forms.ModelForm):
+    name = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class': 'input input-bordered w-full',
+            'placeholder': 'Contact Name'
+        })
+    )
+
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={
+            'class': 'input input-bordered w-full',
+            'placeholder': 'Email Address'
+        })
+    )    
+
+    def clean_name(self):
+        name = self.cleaned_data['name']
+        if name.startswith('X'):
+            raise forms.ValidationError("Name cannot start with a X!")
+        return name
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        # Ensure email is unique
+        if Contact.objects.filter(user=self.initial.get('user'), email=email).exists():
+            raise forms.ValidationError("A contact with this email already exists.")
+        return email
+
+    class Meta:
+        model = Contact
+        fields = (
+            'name', 'email'
+        )
--- contacts/views.py
+++ contacts/views.py
@@ -1,6 +1,53 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+import time
+from contacts.forms import ContactForm
+from django.views.decorators.http import require_http_methods
 
-# Create your views here.
+@login_required
 def index(request):
-    context = {}
+    contacts = request.user.contacts.all().order_by('-created_at')
+    context = {
+        'contacts': contacts,
+        'form': ContactForm()        
+        }
     return render(request, 'contacts.html', context)
+
+@login_required
+def search_contacts(request):
+    query = request.GET.get('search', '')
+    time.sleep(1)
+
+    # use the query to filter contacts by name or email
+    contacts = request.user.contacts.filter(
+        Q(name__icontains=query) | Q(email__icontains=query)
+    )
+    return render(
+        request,
+        'partials/contact-list.html', 
+        #'contacts.html',
+        {'contacts': contacts}
+    )
+
+@login_required
+@require_http_methods(['POST'])
+def create_contact(request):
+    form = ContactForm(request.POST, initial={'user': request.user})
+    if form.is_valid():
+        contact = form.save(commit=False)
+        contact.user = request.user
+        contact.save()
+        # return partial containing a new row for our user
+        # that we can add to the table
+        context = {'contact': contact}
+        response = render(request, 'partials/contact-row.html', context)  
+        response['HX-Trigger'] = 'success'  
+        return response
+    
+    else:
+        response = render(request, 'partials/add-contact-modal.html', {'form': form})
+        response['HX-Retarget'] = '#contact_modal'
+        response['HX-Reswap'] = 'outerHTML'
+        response['HX-Trigger-After-Settle'] = 'fail'
+        return response
